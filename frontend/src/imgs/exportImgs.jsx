52e4a6337d67007fa64ae47b0d3285515a9b4192
Fabricio Rodrigues <fabricio12nastacio@gmail.com>
2024-12-17 14:29:09 -0300

    Adiciona algumas imagens e novos itens a base de dados do cardapio

---
+++ frontend/src/imgs/exportImgs.jsx
@@ -0,0 +1,39 @@
+import caipirinha from './drinks/caipirinha.jpg';
+import caipVodkaMorango from './drinks/caipVodkaM.jpg';
+import campari from './drinks/campari.jpg';
+import coronita from './drinks/coronita.jpg';
+import mojito from './drinks/mojito.jpg';
+import negroni from './drinks/negroni.jpg';
+import ginTonica from './drinks/ginTonica.jpg';
+
+import picanha from './foods/picanha.jpeg';
+import bttaFrita from './foods/batataFrita.jpg';
+import bolinhoCarne from './foods/bolinhoCarne.jpg';
+import bolinhoCostela from './foods/bolinhoCostela.png';
+import carnePanela from './foods/carnePanela.jpg';
+import carneSol from './foods/carneSol.jpg';
+import coxinhaCostela from './foods/coxinhaCastela.jpg';
+import coxinhaPernil from './foods/coxinhaPernil.jpg';
+import fileMignonGorgonzola from './foods/fileMignonGorgonzola.jpg';
+
+export const drinks = {
+  caipirinha,
+  caipVodkaMorango,
+  campari,
+  coronita,
+  mojito,
+  negroni,
+  ginTonica,
+};
+
+export const foods = {
+  picanha,
+  bttaFrita,
+  bolinhoCarne,
+  bolinhoCostela,
+  carnePanela,
+  carneSol,
+  coxinhaCostela,
+  coxinhaPernil,
+  fileMignonGorgonzola,
+};
