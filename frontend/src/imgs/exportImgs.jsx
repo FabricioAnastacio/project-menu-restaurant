cc8f8ade5755ef85d742c2bb3e16dc673f116733
Fabricio Rodrigues <fabricio12nastacio@gmail.com>
2025-01-11 16:25:12 -0300

    Adiciona novos dados ao banco de itens do cardapio

--- frontend/src/imgs/exportImgs.jsx
+++ frontend/src/imgs/exportImgs.jsx
@@ -5,6 +5,13 @@ import coronita from './drinks/coronita.jpg';
 import mojito from './drinks/mojito.jpg';
 import negroni from './drinks/negroni.jpg';
 import ginTonica from './drinks/ginTonica.jpg';
+import lagoaAzul from './drinks/lagoaAzul.jpg';
+import marguerita from './drinks/marguerita.jpg';
+import vinho from './drinks/vinho.jpg';
+import vodka from './drinks/vodka.jpg';
+import redlabel from './drinks/redlabel.jpg';
+import blacklabel from './drinks/blacklabel.jpg';
+import jackdaniels from './drinks/jackdaniels.jpg';
 
 import picanha from './foods/picanha.jpeg';
 import bttaFrita from './foods/batataFrita.jpg';
@@ -24,6 +31,13 @@ export const drinks = {
   mojito,
   negroni,
   ginTonica,
+  lagoaAzul,
+  marguerita,
+  vinho,
+  vodka,
+  redlabel,
+  blacklabel,
+  jackdaniels,
 };
 
 export const foods = {
