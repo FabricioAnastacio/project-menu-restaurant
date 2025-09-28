e43ffdd3cb133a97575265c06b74162ae201c869
Fabricio <fabricio12nastacio@gmail.com>
2025-09-28 13:05:48 -0300

    Adiciona o botão para voltar ao cardapio

--- frontend/src/components/FooterRotes.jsx
+++ frontend/src/components/FooterRotes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import iconsCart from '../pictures/icons8-fast-cart-96.png';
+import iconsFastFood from '../pictures/icons8-fast-food-96.png';
 import '../style/footerRotes.css';
 
 class FooterRotes extends React.Component {
@@ -9,7 +10,12 @@ class FooterRotes extends React.Component {
       <section className="footer-pages">
         <ul>
           <li>
-            <Link to="/cart" className="cart">
+            <Link to="/" className="iconRote">
+              <img src={ iconsFastFood } alt="Hamburguer" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart" className="iconRote">
               <img src={ iconsCart } alt="Carrinho" />
             </Link>
           </li>
