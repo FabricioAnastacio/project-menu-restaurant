9bc44ea681cf7444fa25afce34e2e854e750c770
Fabricio <fabricio12nastacio@gmail.com>
2025-09-27 21:48:50 -0300

    Primeiro Context API com POO

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,28 @@ import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import MenuPage from './pages/MenuPage';
 import Avaliations from './pages/AvaliationsPage';
+import Cart from './pages/Cart';
+import AppContext from './context/AppContext';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      listCart: [],
+    };
+  }
+
   render() {
+    const { listCart } = this.state;
     return (
-      <Routes>
-        <Route exact path="/" Component={ MenuPage } />
-        <Route exact path="/avaliation" Component={ Avaliations } />
-      </Routes>
+      <AppContext.Provider value={ listCart }>
+        <Routes>
+          <Route exact path="/" Component={ MenuPage } />
+          <Route exact path="/cart" Component={ Cart } />
+          <Route exact path="/avaliation" Component={ Avaliations } />
+        </Routes>
+      </AppContext.Provider>
     );
   }
 }
---
+++ frontend/src/context/AppContext.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const listCart = [];
+
+const AppContext = React.createContext(listCart);
+
+export default AppContext;
