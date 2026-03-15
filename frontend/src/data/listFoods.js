e8bf7d4d1a1938575ac8a9083ff8cba9d94fd314
Fabricio <fabricio12nastacio@gmail.com>
2026-03-15 18:11:48 -0300

    Corrige alterações

--- frontend/src/data/listFoods.js
+++ frontend/src/data/listFoods.js
@@ -50,11 +50,7 @@ const listFoods = {
       img: foods.fenix,
       name: 'Fênix Burger',
       description: 'Das chamas nasce um sabor impossível de derrotar. A cada mordida, a vontade de comer mais é forte.',
-<<<<<<< HEAD
-      ingredients: ['Pão', ' Bife 90g', ' Frango desfiado', ' Requeijão cremoso', ' Calabresa', ' Mussarela', ' Salada', ' Milho e batata'],
-=======
       ingredients: ['Pão', ' Bife 90g', ' Mussarela', ' Requeijão cremoso', ' Frango desfiado', ' Calabresa', ' Salada', ' Milho e batata'],
->>>>>>> refs/remotes/origin/main
       value: 25.00,
       amount: 0,
     },
@@ -62,15 +58,9 @@ const listFoods = {
       id: 6,
       obs: '',
       img: foods.natureza,
-<<<<<<< HEAD
-      name: 'Beleza da Natureza',
-      description: 'Pão, Ovo, Mussarela, Salada, Milho e batata',
-      ingredients: ['VEGETARIANO', 'Pão', ' Ovo', ' Cebola roxa', ' Mussarela', ' Salada', ' Milho e batata'],
-=======
       name: 'Beleza da Natureza VEGETARIANO',
       description: 'Pão, 2 Ovos, Mussarela, Cebola roxa, Salada, Milho e batata',
       ingredients: ['Pão', ' 2 Ovos', ' Mussarela', ' Cebola roxa', ' Salada', ' Milho e batata'],
->>>>>>> refs/remotes/origin/main
       value: 14.50,
       amount: 0,
     },
@@ -80,11 +70,7 @@ const listFoods = {
       img: foods.bigTanjiro,
       name: 'Big Tanjiro',
       description: 'Determinação que corta qualquer fome. Gigante. Honrado. A vontade de matar sua fome é Imparável.',
-<<<<<<< HEAD
-      ingredients: ['Pão', ' 2 Bifes 90g', ' Frango desfiado', ' Requeijão cremoso', ' 2 Ovos', ' Bacon', ' Presunto', ' Mussarela', ' Salada', ' Milho e batata'],
-=======
       ingredients: ['Pão', ' 2 Bifes 90g', ' Mussarela', ' Requeijão cremoso', ' Bacon', ' Calabresa', ' Frango desfiado', ' Presunto', 'Ovo', ' Salada', ' Milho e batata'],
->>>>>>> refs/remotes/origin/main
       value: 30.00,
       amount: 0,
     },
@@ -94,11 +80,7 @@ const listFoods = {
       img: foods.babyStitch,
       name: 'Baby Stitch ARTESANAL',
       description: 'Pequeno no tamanho. Caótico no sabor. Uma explosão inesperada a cada mordida.',
-<<<<<<< HEAD
-      ingredients: ['Pão brioche', ' Blend bovino 120g', ' Bacon fatiado', ' Cheddar e molho especial', ' (ACOMPANHA FRITAS POKÊ 150G)'],
-=======
       ingredients: ['Pão brioche', ' Blend bovino 120g', ' Cheddar', ' Bacon fatiado', ' Molho especial', ' (ACOMPANHA FRITAS POKÊ 150G)'],
->>>>>>> refs/remotes/origin/main
       value: 25.00,
       amount: 0,
     },
@@ -108,28 +90,17 @@ const listFoods = {
       img: foods.titanBlind,
       name: 'Titan Blindado ARTESANAL',
       description: 'Blindagem ativada. Impacto liberado. Estrutura pesada. Presença dominante.',
-<<<<<<< HEAD
-      ingredients: ['Pão brioche', ' Blend bovino 120g', ' Bacon fatiado', ' Mussarela', ' Cheddar', ' Cebola roxa e molho especial', ' (ACOMPANHA KATANA BATATA 150G)'],
-=======
       ingredients: ['Pão brioche', ' Blend bovino 120g', ' Mussarela', ' Cheddar', ' Bacon fatiado', ' Cebola roxa', ' Molho especial', ' (ACOMPANHA KATANA BATATA 150G)'],
->>>>>>> refs/remotes/origin/main
       value: 27.00,
       amount: 0,
     },
     {
       id: 10,
       obs: '',
-<<<<<<< HEAD
-      img: foods.tatanColoss,
-      name: 'Titan Colossal ARTESANAL',
-      description: 'Quando ele surge… tudo para. Gigante, carregado e absolutamente destruidor de fome.',
-      ingredients: ['Pão brioche', '2 Blends bovinos 120g', 'Queijo empanado crocante', 'Bacon fatiado', 'Cheddar', 'Salada', 'Cebola roxa e molho especial', ' (ACOMPANHA KATANA BATATA 150G)'],
-=======
       img: foods.titanColoss,
       name: 'Titan Colossal ARTESANAL',
       description: 'Quando ele surge… tudo para. Gigante, carregado e absolutamente destruidor de fome.',
       ingredients: ['Pão brioche', ' 2 Blends bovinos 120g', ' Cheddar', ' Queijo empanado crocante', ' Bacon fatiado', ' Cebola roxa', ' Alface', ' Tomate', ' Molho especial barbecue', ' (ACOMPANHA KATANA BATATA 150G)'],
->>>>>>> refs/remotes/origin/main
       value: 35.00,
       amount: 0,
     },
