4ef5bd005cc9bdb08c7bd3aa503141b23fe12a53
Fabricio_Rodrigues <fabricio12nastacio@gmail.com>
2026-05-04 15:06:31 -0300

    Atualizar o listHighlights.js Remove a programação do Comunicado

--- frontend-menu/src/data/listHighlights.js
+++ frontend-menu/src/data/listHighlights.js
@@ -7,7 +7,7 @@ const listhighlights = [
   {
     id: 1,
     obs: '',
-    displayDate: [0, 3, 4, 5, 6],
+    displayDate: [],
     img: foods.comunicado,
     name: 'Comunicado',
     nameHighlights: 'COMUNICADO',
