f1ba25945bac686c60caea45cc97612fb88919bd
Fabricio <fabricio12nastacio@gmail.com>
2026-08-18 16:18:57 -0300

    Configura o database config e corrige o tsconfig para trabalhar com NodeNext e ESM

---
+++ backend/src/database/config/database.ts
@@ -0,0 +1,14 @@
+import type { Options } from 'sequelize';
+
+export const config: Options = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '679301',
+  database: 'DB_MENU_DELIVERY', 
+  host: process.env.DB_HOST || 'dbdlibre',
+  port: Number(process.env.DB_PORT) || 3306,
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+};
